7ca7b4f27069784e67dcef9ba6295dbf5bd42370
chaesuhyeon <ddd8177@gmail.com>
2022-08-15 23:27:20 +0900

    부호 연산자

--- Basic/Ex3_4.java
+++ Basic/Ex3_4.java
@@ -1,11 +1,11 @@
 public class Ex3_4 {
     public static void main(String[] args) {
         int i = -10;
-        i = +i;
+        i = +i; // 양수
         System.out.println(i);
 
         i = -10;
-        i = -i;
+        i = -i; // 음수
         System.out.println(i);
     }
 }
