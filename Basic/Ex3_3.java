8dbc967bd7b8256ff7f568b56e294c81c5fc3856
suhyeon <ddd8177@sweetk.co.kr>
2022-08-15 23:09:59 +0900

    증감 연산자

---
+++ Basic/Ex3_3.java
@@ -0,0 +1,17 @@
+public class Ex3_3 {
+    public static void main(String[] args) {
+        int i = 5, j = 5;
+        System.out.println(i++);
+        System.out.println(++j);
+        System.out.println("i = " + i + "j = " + j);
+
+        // System.out.println(i);
+        // i++;
+        // ++j;
+        // System.out.println(j);
+
+//        5
+//        6
+//        i = 6j = 6
+    }
+}
