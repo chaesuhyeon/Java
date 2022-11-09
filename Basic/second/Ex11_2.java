bdb6bcdd389c26cb0921bd5d22bee6b7199fad21
suhyeon <ddd8177@gmail.com>
2022-11-09 13:03:47 +0900

    스택, 큐

---
+++ Basic/second/Ex11_2.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Ex11_2 {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        Queue q = new LinkedList();
+
+        st.push("0");
+        st.push("1");
+        st.push("2");
+
+        q.offer("0");
+        q.offer("1");
+        q.offer("2");
+
+        System.out.println("= Stack =");
+        while (!st.empty()){
+            System.out.println(st.pop()); // 스택에서 요소 하나를 꺼내서 출력
+        }
+
+        System.out.println("= Queue =");
+        while (!q.isEmpty()){
+            System.out.println(q.poll()); // 큐에서 요소 하나를 꺼내서 출력
+        }
+    }
+}
+/*
+= Stack =
+2
+1
+0
+= Queue =
+0
+1
+2*/
