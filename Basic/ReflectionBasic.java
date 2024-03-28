4a220b2a5d09bf6ee263068a380ce86de4478971
chaesuhyeon <ddd8177@gmail.com>
2024-03-29 07:21:03 +0900

    reflection 간단 예제

---
+++ Basic/ReflectionBasic.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Method;
+
+public class ReflectionBasic {
+
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = Class.forName("Test");
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+    }
+}
+
+class Test {
+    public void method1() {
+        System.out.println("Method 1");
+    }
+
+    public void method2() {
+        System.out.println("Method 2");
+    }
+}
